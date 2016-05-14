bb35d755d25398ea08c7dd927c01440e455fb5b3
jibancanyang <1599664856@qq.com>
2016-05-14 20:31:57 +0800

    jibancanyang

---
+++ dp/zoj3662.cpp
@@ -0,0 +1,60 @@
+/**********************jibancanyang**************************
+ *Author*        :jibancanyang
+ *Created Time*  : 六  5/14 19:54:28 2016
+ *File Name*     : jy.cpp
+**Problem**:
+**Analyse**:
+**Code**:
+***********************1599664856@qq.com**********************/
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <stack>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef unsigned long long ull;
+vector<int> vi;
+#define pr(x) cout << #x << ": " << x << "  " 
+#define pl(x) cout << #x << ": " << x << endl;
+#define pri(a) printf("%d\n",(a));
+#define xx first
+#define yy second
+#define sa(n) scanf("%d", &(n))
+#define sal(n) scanf("%lld", &(n))
+#define sai(n) scanf("%I64d", &(n))
+#define vep(c) for(decltype((c).begin() ) it = (c).begin(); it != (c).end(); it++) 
+const int mod = int(1e9) + 7, INF = 0x3fffffff;
+const int maxn = 1e5 + 13;
+
+
+
+int main(void)
+{
+#ifdef LOCAL
+    //freopen("/Users/zhaoyang/in.txt", "r", stdin);
+    //freopen("/Users/zhaoyang/out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int n; sa(n);
+    map<string, int> mp;
+    while (n--) {
+        string str;
+        cin >> str;
+        sort(str.begin(), str.end());
+        mp[str]++;
+        cout << mp[str] - 1 << endl;
+    }
+    
+    return 0;
+}
