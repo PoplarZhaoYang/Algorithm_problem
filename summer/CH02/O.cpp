4741b0aacc216b9074cd49d96d8559fe79b38d6a
jibancanyang <1599664856@qq.com>
2016-07-08 12:16:07 +0800

    jibancanyang

--- summer/CH02/O.cpp
+++ summer/CH02/O.cpp
@@ -1,11 +1,15 @@
+
 /**********************jibancanyang**************************
  *Author*        :jibancanyang
- *Created Time*  : 三  7/ 6 14:07:06 2016
+ *Created Time*  : 三  7/ 6 16:11:13 2016
 **Problem**:
 **Analyse**:
-经典的区间dp，首先从1开始枚举区间的长度L，然后枚举左端点i，右端点j为i + L。
-然后当前区间的值要么通过小区间扩展而来，要么通过小区间合并而来。
+二维棋盘随机放置，覆盖所有行列的期望天数。经典概率dp
+定义$dp[i][j]$为已经出现$i$种，有$j$个系统的期望天数。
+那么它是由它的四个子结果的期望天数的加权平均 + 1 天。
+这里的加权平均的权是各自的概率。
 **Get**:
+
 **Code**:
 ***********************1599664856@qq.com**********************/
 
@@ -38,7 +42,7 @@ typedef vector<int> vi;
 #define vep(c) for(decltype((c).begin() ) it = (c).begin(); it != (c).end(); it++) 
 const int mod = int(1e9) + 7, INF = 0x3f3f3f3f;
 const int maxn = 1e5 + 13;
-int dp[111][111];
+double dp[1009][1009];
 
 
 
@@ -48,21 +52,23 @@ int main(void)
     freopen("in.txt", "r", stdin);
     //freopen("out.txt", "w", stdout);
 #endif
-    string str;
-    while (cin >> str, str != "end") {
-        int n = str.size();
+    int n, s;
+    while (~scanf("%d%d", &n, &s)) {
         memset(dp, 0, sizeof(dp));
-        for (int L = 1; L <= n; L++) {
-            for (int i = 0; i < n; i++) {
-                int j = i + L;
-                if (j >= n) break;
-                if ((str[i] == '[' && str[j] == ']') || (str[i] == '(' && str[j] == ')')) {
-                    dp[i][j] = max((j - i > 2 ? dp[i + 1][j - 1]  : 0) + 2, dp[i][j]);
-                }
-                for (int k = i ; k < j; k++) dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j]);
+        dp[n][s] = 0;
+        for (int i = n; i >= 0; i--) {
+            for (int j = s; j >= 0; j--) {
+                if (i == n && j == s) continue;
+                dp[i][j] = (
+                     dp[i + 1][j] * (n - i) * j  + 
+                     n * s  +
+                     dp[i][j + 1] * i * (s - j)  + 
+                     dp[i + 1][j + 1] * (n - i) * (s - j) ) 
+                    / (n * s - i * j);
             }
         }
-        pri(dp[0][n - 1]);
+        printf("%.4f\n", dp[0][0]);
     }
+
     return 0;
 }
--- summer/CH02/k.cpp
+++ summer/CH02/k.cpp
@@ -1,11 +1,13 @@
 /**********************jibancanyang**************************
  *Author*        :jibancanyang
- *Created Time*  : 三  7/ 6 14:07:06 2016
+ *Created Time*  : 三  7/ 6 11:18:04 2016
 **Problem**:
 **Analyse**:
-经典的区间dp，首先从1开始枚举区间的长度L，然后枚举左端点i，右端点j为i + L。
-然后当前区间的值要么通过小区间扩展而来，要么通过小区间合并而来。
+最小满足某种条件的最小区间，典型双指针应用.
+
 **Get**:
+双指针的模板写法，最外层循环不是右边界小于n，而是true因为之后还可能有更优的出现，里面先更新右边界，然后取值，然后更新左边界。
+
 **Code**:
 ***********************1599664856@qq.com**********************/
 
@@ -37,9 +39,8 @@ typedef vector<int> vi;
 #define sai(n) scanf("%I64d", &(n))
 #define vep(c) for(decltype((c).begin() ) it = (c).begin(); it != (c).end(); it++) 
 const int mod = int(1e9) + 7, INF = 0x3f3f3f3f;
-const int maxn = 1e5 + 13;
-int dp[111][111];
-
+const int maxn = 1e6 + 13;
+int A[maxn];
 
 
 int main(void)
@@ -48,21 +49,27 @@ int main(void)
     freopen("in.txt", "r", stdin);
     //freopen("out.txt", "w", stdout);
 #endif
-    string str;
-    while (cin >> str, str != "end") {
-        int n = str.size();
-        memset(dp, 0, sizeof(dp));
-        for (int L = 1; L <= n; L++) {
-            for (int i = 0; i < n; i++) {
-                int j = i + L;
-                if (j >= n) break;
-                if ((str[i] == '[' && str[j] == ']') || (str[i] == '(' && str[j] == ')')) {
-                    dp[i][j] = max((j - i > 2 ? dp[i + 1][j - 1]  : 0) + 2, dp[i][j]);
-                }
-                for (int k = i ; k < j; k++) dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j]);
-            }
+    int n;
+    while (~sa(n)) {
+        set<int> st;
+        for (int i = 0; i < n; i++) {
+            sa(A[i]);
+            st.insert(A[i]);
         }
-        pri(dp[0][n - 1]);
+        int l = 0, r = 0, k = st.size();
+        map<int ,int> mp;
+        int ans = n;
+        int cnt = 0;
+        while (true) {
+            while (cnt < k && r < n) {
+                if (mp[A[r++]]++ == 0) cnt++;
+            }
+            if (cnt >= k) ans = min(ans, r - l);
+            else break;
+            if (--mp[A[l++]] == 0) cnt--; 
+        }
+        pri(ans);
     }
+
     return 0;
 }
