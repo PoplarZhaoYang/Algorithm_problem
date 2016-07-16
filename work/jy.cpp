1f434f087d3f624dfb3570e106da78a739f1effd
jibancanyang <1599664856@qq.com>
2016-07-16 09:40:13 +0800

    jibancanyang

--- work/jy.cpp
+++ work/jy.cpp
@@ -1,58 +1,20 @@
-/**********************jibancanyang**************************
- *Author*        :jibancanyang
- *Created Time*  : 五  7/15 14:33:22 2016
-**Problem**:
-**Analyse**:
-**Get**:
-**Code**:
-***********************1599664856@qq.com**********************/
-
-#include <cstdio>
-#include <cstring>
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
-#include <string>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <stack>
 using namespace std;
-typedef pair<int, int> pii;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<int> vi;
-#define pr(x) cout << #x << ": " << x << "  " 
-#define pl(x) cout << #x << ": " << x << endl;
-#define pri(a) printf("%d\n",(a))
-#define xx first
-#define yy second
-#define sa(n) scanf("%d", &(n))
-#define sal(n) scanf("%lld", &(n))
-#define sai(n) scanf("%I64d", &(n))
-#define vep(c) for(decltype((c).begin() ) it = (c).begin(); it != (c).end(); it++) 
-const int mod = int(1e9) + 7, INF = 0x3f3f3f3f;
-const int maxn = 1e5 + 13;
+
+template <typename T>
+class testClass {
+public:
+    static int _data;
+};
+
+template <>
+int testClass<int>::_data = 1;
 
 
-
-int main(void)
+int main()
 {
-#ifdef LOCAL
-    freopen("in.txt", "r", stdin);
-    //freopen("out.txt", "w", stdout);
-#endif
-    int n, z = 0;
-    sa(n);
-    for (int i = 0; i < n; i++) {
-        int x; sa(x);
-        if (!x) z++;
-    }
-    if (n == 1 && z == 0) puts("YES");
-    else if (n > 1 && z == 1) puts("YES");
-    else puts("NO");
+    cout << testClass<int>::_data << endl;
+    testClass<char> ch;
+    cout << ch._data << endl;
     return 0;
 }
